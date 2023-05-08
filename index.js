bfe6f88afec398d461edbd685cf8304c48af2afd
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-08 16:41:07 -0700

    added basic elements of HTML file and set up CSS/JS sheets with links and pseudo-code

---
+++ index.js
@@ -0,0 +1,12 @@
+// there need to be variables to show answers right/wrong as well as a highscore table to track the most answered correctly in a run
+// there needs to be a 30 second timer that starts when a new question appears on screen
+// if the timer runs out then the question is marked wrong and automatically goes to next question
+// there should be a start button to initiate the quiz, and each multiple choice has a button that is either "wrong" or "correct"
+
+
+// maybe make each question a variable with set attributes like the correct answer in order for function to check
+// var question1 = {
+    question: "bsdssfdjgznkj"
+    answer: "awshbsldg"
+// }
+// then have a function that calls for question1.question and an if statement that says is event.target = question1.answer then print "correct!" etc
